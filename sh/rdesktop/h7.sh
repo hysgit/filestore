7c9c2adf7a33ebb77327427c128fe6248f473f5f
huangyue <huangyue306@gmail.com>
2017-10-18 10:22:52 +0800

    update

---
+++ sh/rdesktop/h7.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+#rdesktop -u wangbin 10.248.1.104 -g 1900x1000 -pQWer1234  -a 32 -r clipboard:PRIMARYCLIPBOARD -r sound:local
+#rdesktop -u wangbin 10.248.1.104 -g workarea -D -z -pQWer1234  -a 32 -r clipboard:PRIMARYCLIPBOARD -r sound:local
+rdesktop -u hy 192.168.0.7 -g 1366x738 -z -p 1 -b -D  -N -a 32 -r clipboard:PRIMARYCLIPBOARD -r sound:local 
