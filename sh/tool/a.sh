6c0f88893909f9a3bef0eefde83aeebfdb34e973
huangyue <huangyue306@gmail.com>
2017-11-10 17:54:10 +0800

    update

---
+++ sh/tool/a.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+ip=1.1.1.1
+ip2=1.1.1.2
+
+if [ "$ip" != "$ip2" ];then
+    echo "不想等"
+fi
---
+++ sh/tool/monitip.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "脚本启动!"
+ip=`nslookup officevpn.91ganlu.com | tail -n 2 | head -n 1 | awk '{print $2}'`
+i=1
+while((i<=100))
+do
+    ip2=`nslookup officevpn.91ganlu.com | tail -n 2 | head -n 1 | awk '{print $2}'`
+    if [ "$ip" != "$ip2" ];then
+        docker restart nginx-gw-v0829-withcache
+        echo "ip地址发生变化,重启nginx"
+        echo "原ip:${ip}"
+        echo "新IP:${ip2}"
+        ip=ip2
+    fi
+done
