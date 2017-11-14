0e23d8e330e6eb4a0bb61320d857006c5026ec0e
huangyue <huangyue306@gmail.com>
2017-11-14 18:00:03 +0800

    save

--- sh/tool/monitip.sh
+++ sh/tool/monitip.sh
@@ -5,11 +5,13 @@ i=1
 while((i<=100))
 do
     ip2=`nslookup officevpn.91ganlu.com | tail -n 2 | head -n 1 | awk '{print $2}'`
-    if [ "$ip" != "$ip2" ];then
+    if [ "${ip}" != "${ip2}" ];then
+        echo $(date +%Y%m%d%H%M)
         docker restart nginx-gw-v0829-withcache
         echo "ip地址发生变化,重启nginx"
         echo "原ip:${ip}"
         echo "新IP:${ip2}"
+        echo ""
         ip=${ip2}
     fi
     sleep 3
