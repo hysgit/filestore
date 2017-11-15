b756f58699d87081ce1c056a503587713214e7c2
huangyue <huangyue306@gmail.com>
2017-11-15 16:51:00 +0800

    save

--- sh/tool/monitip.sh
+++ sh/tool/monitip.sh
@@ -5,6 +5,7 @@ i=1
 while((i<=100))
 do
     ip2=`nslookup officevpn.91ganlu.com | tail -n 2 | head -n 1 | awk '{print $2}'`
+    echo "${ip2}"
     if [ "${ip}" != "${ip2}" ];then
         echo $(date +%Y%m%d%H%M)
         docker restart nginx-gw-v0829-withcache
